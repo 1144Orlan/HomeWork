c85747e9dba32f580797b99dc1eba3b783c25d7b
Project1144 <74624202+Project1144@users.noreply.github.com>
2024-06-09 02:33:45 +0300

    Add files via upload

---
+++ Lab9/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GraphicShapesHierarchy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle okrujnost = new Circle(5);
+            okrujnost.OutSidesOnScreen();
+            double dlinaOkr = okrujnost.CircleLength();
+            double ploshadOkr = okrujnost.CircleArea();
+            Console.WriteLine($"Длина окружности с радиусом: {okrujnost.r} равна {dlinaOkr:#.00}");
+            Console.WriteLine($"Площадь круга с радиусом: {okrujnost.r} равна {ploshadOkr:#.00}");
+
+            Square kvadrat = new Square(10);
+            kvadrat.OutSidesOnScreen();
+            double perimeterKvadrata = kvadrat.PerimetrKvadrata();
+            double ploshadKvadrata = kvadrat.PloshadKvadrata();
+            Console.WriteLine($"Периметр квадрата со стороной: {kvadrat.a} равен {perimeterKvadrata}");
+            Console.WriteLine($"Площадь квадрата со стороной: {kvadrat.a} равна {ploshadKvadrata:#.00}");
+            kvadrat.Rotate();
+
+            Console.Write("\nВведите сторону 'a' треугольника: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите сторону 'b': ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите сторону 'c': ");
+            double c = double.Parse(Console.ReadLine());
+            Triangle tr1 = new Triangle(a, b, c); // треугольник abc
+
+            double perimeter = tr1.PerimetrTreug();
+            double squareTriangle = tr1.TriangleSquare(a, b, c);
+            Console.WriteLine($"Периметр треугольника со сторонами: {tr1.a}, {tr1.b} и {tr1.c} равен {perimeter}");
+            Console.WriteLine($"Площадь треугольника равна {squareTriangle:#.00}");
+            tr1.OutSidesOnScreen();
+            tr1.TriangleExistence();
+            tr1.Rotate();
+
+            Console.Write("\nВведите сторону равностороннего треугольника: ");
+            c = double.Parse(Console.ReadLine());
+            Triangle tr2 = new Triangle(c); // равносторонний треугольник
+            perimeter = tr2.PerimetrTreug();
+            squareTriangle = tr2.TriangleSquare(c);
+            Console.WriteLine($"Площадь треугольника равна {squareTriangle:#.00}");
+            tr2.Rotate();
+        }
+    }
+}
---
+++ Lab9/Triangle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace GraphicShapesHierarchy
+{
+    internal class Triangle : Shape, IFigureRotation //наследование классу, интерфейс
+    {         
+            public double a;
+            public double b;
+            public double c;
+
+            public Triangle(double a, double b, double c) : this(c) // для создания треугольника с заданными длинами сторон
+            {
+                this.a = a;
+                this.b = b;
+            }
+            public Triangle(double c) //для создания равностороннего треугольника
+            {
+                this.c = c;
+            }
+            public void Rotate() //реализация интерфейса
+        {
+            Console.WriteLine("Треугольник повёрнут");
+        }
+            public double PerimetrTreug()
+            {
+                double perimetr = (a + b + c);
+                return perimetr;
+            }
+            public double PoluPerimetrTreug()
+            {
+                double poluPerimetr = (a + b + c) / 2;
+                return poluPerimetr;
+            }
+            public double TriangleSquare( double a, double b, double c)
+            {
+                double p = PoluPerimetrTreug();
+                double square = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return square;
+            }
+            public double TriangleSquare(double a) //равносторонний треугольник
+            {
+                double b = a;
+                double c = a;
+                double p = (a + b + c) / 2;
+                double square = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return square;
+            }
+            public override void OutSidesOnScreen()
+            {
+                Console.WriteLine($"Длины сторон треугольника, если забыли: a: {a}, b: {b}, c: {c}");
+            }
+            public void TriangleExistence()
+            {
+                if (a > 0 & b > 0 & c > 0 & a + c > b & b + c > a & a + b > c)
+                {
+                    // вычисление знаков косинусов
+                    double cosC = a * a + b * b - c * c;
+                    double cosA = c * c + b * b - a * a;
+                    double cosB = a * a + c * c - b * b;
+                    // анализ треугольника
+                    if (cosC < 0 | cosA < 0 | cosB < 0)
+                        Console.WriteLine("Это тупоугольный треугольник");
+                    else
+                    if (cosC > 0 & cosA > 0 & cosB > 0)
+                        Console.WriteLine("Это остроугольный треугольник");
+                    else
+                    if (cosC == 0 | cosA == 0 & cosB == 0)
+                        Console.WriteLine("Это прямоугольный треугольник");
+                }
+                else
+                    Console.WriteLine("Это не треугольник");
+            }        
+    }
+}
