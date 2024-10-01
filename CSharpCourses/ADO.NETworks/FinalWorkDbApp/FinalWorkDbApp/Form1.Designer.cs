147c08c50e19015dc57f1df0d8b232e7ce7a4d64
1144Orlan <74624202+1144Orlan@users.noreply.github.com>
2024-10-02 00:08:45 +0300

    Simple app ADO.NET&WinForms

---
+++ CSharpCourses/ADO.NETworks/FinalWorkDbApp/FinalWorkDbApp/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace FinalWorkDbApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sqlServerConnection = new Microsoft.Data.SqlClient.SqlConnection();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.column1TextBox = new System.Windows.Forms.TextBox();
+            this.column2TextBox = new System.Windows.Forms.TextBox();
+            this.labelCompanyID = new System.Windows.Forms.Label();
+            this.labelCompanyName = new System.Windows.Forms.Label();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.sqlServerDataAdapter = new Microsoft.Data.SqlClient.SqlDataAdapter();
+            this.sqlCommand1 = new Microsoft.Data.SqlClient.SqlCommand();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sqlServerConnection
+            // 
+            this.sqlServerConnection.AccessTokenCallback = null;
+            this.sqlServerConnection.ConnectionString = "Data Source=(local);Initial Catalog=Northwind;Integrated Security=True";
+            this.sqlServerConnection.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(48, 34);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameTextBox.TabIndex = 0;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(48, 83);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 1;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(48, 13);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(58, 13);
+            this.labelUsername.TabIndex = 2;
+            this.labelUsername.Text = "Username:";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(48, 61);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(56, 13);
+            this.labelPassword.TabIndex = 3;
+            this.labelPassword.Text = "Password:";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(48, 118);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(85, 23);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect to DB";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(48, 158);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(365, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // column1TextBox
+            // 
+            this.column1TextBox.Location = new System.Drawing.Point(48, 349);
+            this.column1TextBox.Name = "column1TextBox";
+            this.column1TextBox.Size = new System.Drawing.Size(100, 20);
+            this.column1TextBox.TabIndex = 6;
+            // 
+            // column2TextBox
+            // 
+            this.column2TextBox.Location = new System.Drawing.Point(48, 391);
+            this.column2TextBox.Name = "column2TextBox";
+            this.column2TextBox.Size = new System.Drawing.Size(100, 20);
+            this.column2TextBox.TabIndex = 7;
+            // 
+            // labelCompanyID
+            // 
+            this.labelCompanyID.AutoSize = true;
+            this.labelCompanyID.Location = new System.Drawing.Point(50, 330);
+            this.labelCompanyID.Name = "labelCompanyID";
+            this.labelCompanyID.Size = new System.Drawing.Size(68, 13);
+            this.labelCompanyID.TabIndex = 2;
+            this.labelCompanyID.Text = "Company ID:";
+            // 
+            // labelCompanyName
+            // 
+            this.labelCompanyName.AutoSize = true;
+            this.labelCompanyName.Location = new System.Drawing.Point(50, 375);
+            this.labelCompanyName.Name = "labelCompanyName";
+            this.labelCompanyName.Size = new System.Drawing.Size(83, 13);
+            this.labelCompanyName.TabIndex = 3;
+            this.labelCompanyName.Text = "Company name:";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(213, 31);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 8;
+            this.AddButton.Text = "ADD";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(213, 62);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+            this.UpdateButton.TabIndex = 8;
+            this.UpdateButton.Text = "UPDATE";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(213, 91);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 8;
+            this.DeleteButton.Text = "DELETE";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // sqlCommand1
+            // 
+            this.sqlCommand1.CommandTimeout = 30;
+            this.sqlCommand1.EnableOptimizedParameterBinding = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(443, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Insert actual data into fields \"Company ID\", \"Company name\" before pressing updat" +
+    "e button.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.column2TextBox);
+            this.Controls.Add(this.column1TextBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.labelCompanyName);
+            this.Controls.Add(this.labelCompanyID);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Name = "Form1";
+            this.Text = "Зачетное задание ADO.NET";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Data.SqlClient.SqlConnection sqlServerConnection;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox column1TextBox;
+        private System.Windows.Forms.TextBox column2TextBox;
+        private System.Windows.Forms.Label labelCompanyID;
+        private System.Windows.Forms.Label labelCompanyName;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private Microsoft.Data.SqlClient.SqlDataAdapter sqlServerDataAdapter;
+        private Microsoft.Data.SqlClient.SqlCommand sqlCommand1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CSharpCourses/ADO.NETworks/FinalWorkDbApp/FinalWorkDbApp/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace FinalWorkDbApp
+{
+    public partial class Form1 : Form
+    {
+        private DataTable dataTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = $"Data Source=(local);Initial Catalog=Northwind;Integrated Security=True;TrustServerCertificate=True;;User Id={usernameTextBox.Text};Password={passwordTextBox.Text};";
+
+            try
+            {
+                sqlServerConnection = new SqlConnection(connectionString);
+                sqlServerConnection.Open();
+                MessageBox.Show("Connected successfully!");
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"sqlServerConnection failed: {ex.Message}");
+            }
+
+        }
+
+        private void LoadData()
+        {
+            string query = "SELECT CustomerID, CompanyName FROM Northwind.dbo.Customers";
+            sqlServerDataAdapter = new SqlDataAdapter(query, sqlServerConnection);
+            dataTable = new DataTable();
+            sqlServerDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRow newRow = dataTable.NewRow();
+                newRow["CustomerID"] = column1TextBox.Text;
+                newRow["CompanyName"] = column2TextBox.Text;
+
+                dataTable.Rows.Add(newRow);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(sqlServerDataAdapter);
+                sqlServerDataAdapter.Update(dataTable);
+
+                MessageBox.Show("Data added successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error adding data: {ex.Message}");
+            }
+
+        }
+        
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataGridViewRow selectedRow = dataGridView1.CurrentRow;
+                if (selectedRow != null)
+                {
+                    selectedRow.Cells["CustomerID"].Value = column1TextBox.Text;
+                    selectedRow.Cells["CompanyName"].Value = column2TextBox.Text;
+                    
+                    SqlCommandBuilder commandBuilder = new SqlCommandBuilder(sqlServerDataAdapter);
+                    sqlServerDataAdapter.Update(dataTable);
+
+                    MessageBox.Show("Data updated successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating data: {ex.Message}");
+            }
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.CurrentRow != null)
+                {
+                    dataGridView1.Rows.RemoveAt(dataGridView1.CurrentRow.Index);
+                    SqlCommandBuilder commandBuilder = new SqlCommandBuilder(sqlServerDataAdapter);
+                    sqlServerDataAdapter.Update(dataTable);
+
+                    MessageBox.Show("Data deleted successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error deleting data: {ex.Message}");
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (sqlServerConnection != null && sqlServerConnection.State == ConnectionState.Open)
+            {
+                sqlServerConnection.Close();
+            }
+
+        }
+        }
+        }
